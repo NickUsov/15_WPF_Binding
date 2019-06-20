cdf1ad0eefad624c0a0cc389e2471d33115f34ab
NickUsov <nicky.yalta.ua@gmail.com>
2019-06-21 00:19:01 +0300

    full XAML, used windows resources

---
+++ 15_WPF_Binding/15_WPF_Binding/IsCheckedToFontStyleConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace _15_WPF_Binding
+{
+    class IsCheckedToFontStyleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value == true)
+                return FontStyles.Italic;
+            return FontStyles.Normal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return FontStyles.Normal;
+        }
+    }
+}
